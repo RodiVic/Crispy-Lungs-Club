a161c724b78ad2b4bbc3527c7cfaa0b965fe6b9e
RodiVic <125700212+RodiVic@users.noreply.github.com>
2026-04-19 19:22:27 -0400

    Fix import path for useApi hook in LogTrigger

--- src/components/dashboard/LogTrigger.tsx
+++ src/components/dashboard/LogTrigger.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useApi } from '../../hooks/useapiClient'
+import { useApi } from '../../hooks/apiClient'
 import type { TriggerType } from '../../types'
 
 type Props = {
